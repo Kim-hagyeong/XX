25914f0cc1ebb4ad15e35cf6a26d9d75516f606a
gkrud <gkrud823@naver.com>
2025-04-28 10:37:55 +0900

    new commit

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,7 +1,22 @@
 import express from "express";
 import * as authControllenr from "../controller/auth.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+
+const validateLogin = [
+  body("userid")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("최소 4자 이상 입력")
+    .matches(/^[a-zA-Z0-9]*$/)
+    .withMessage("특수문자는 사용 불가"),
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("최소 8자 이상 입력"),
+];
 
 // 회원가입
 router.post("/signup", authControllenr.signup);
